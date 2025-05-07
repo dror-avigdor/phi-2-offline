4162cc6447a63824c8308675c517c77782a14639
Dror Avigdor <dror.avigdor@gmail.com>
2025-05-07 13:42:11 +0300

    Update test_1.cpp

--- test_1.cpp
+++ test_1.cpp
@@ -1,3 +1,18 @@
-int main(){
-  return 55/0;
-}
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+private:
+    int width;
+    int height;
+
+public:
+    Rectangle(int w, int h) {
+        width = w;
+        height = h
+    }
+
+    int area() {
+        return width / height;
+    }
+};
